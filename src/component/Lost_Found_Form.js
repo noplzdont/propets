27a1f20636ef5a4e3266482da307c78940151f93
Nikita Komliev <nikita.komlyev@gmail.com>
2020-10-09 17:34:31 +0300

    Вёрстка Lost/Found Form.

--- src/component/Lost_Found_Form.js
+++ src/component/Lost_Found_Form.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import style from "../module/lost_found_form.module.css";
-/*import Dropzone from 'react-dropzone';*/
 import IMAGE_SCALE from "../images/image_human_dog_scale.png";
 
 const LostFoundForm = () =>
@@ -14,18 +13,67 @@ const LostFoundForm = () =>
             <div className = {style.div_form_wrapper}>
                 <section className = {style.section_form}>
                     <div className = {style.div_input_side}>
-                        <input/>
-                        <input/>
-                        <input/>
-                        <input/>
-                        <input/>
-                        <input/>
-                        <input/>
-                        <input/>
+                        <div className = {style.div_input_block}>
+                            <label className = {style.input_label} htmlFor = "type">Type:</label>
+                            <select className = {style.select} id = "type">
+                                <option>Dog</option>
+                                <option>Cat</option>
+                            </select>
+                        </div>
+                        <div className = {style.div_input_block}>
+                            <label className = {style.input_label} htmlFor = "sex">Sex:</label>
+                            <select className = {style.select} id = "sex">
+                                <option>Male</option>
+                                <option>Female</option>
+                                <option>Apache Helicopter</option>
+                                <option>Unknown</option>
+                            </select>
+                        </div>
+                        <div className = {style.div_input_block}>
+                            <label className = {style.input_label} htmlFor = "breed">Breed:</label>
+                            <input className = {style.input_form} name = "breed" placeholder = {"Golden Retriever"}/>
+                        </div>
+                        <div className = {style.div_input_block}>
+                            <label className = {style.input_label} htmlFor = "colour">Colour:</label>
+                            <input className = {style.input_form} name = "colour" placeholder = {"Beige"}/>
+                        </div>
+                        <div className = {style.div_input_block}>
+                            <label className = {style.input_label} htmlFor = "height">Height:</label>
+                            <select className = {style.select} id = "height">
+                                <option>20-45 cm</option>
+                                <option>45-70 cm</option>
+                                <option>70-100 cm</option>
+                                <option>100-150 cm</option>
+                            </select>
+                        </div>
+                        <div className = {style.div_input_block}>
+                            <label className = {style.input_label} htmlFor = "features">Distinctive features:
+                                <span className = {style.label_span}> up to 60 chars</span>
+                            </label>
+                            <input className = {style.input_form_feature}
+                                   maxlength = {60}
+                                   name = "features"
+                                   placeholder = "blue collar with stars, no left ear, damaged tail."/>
+                        </div>
+                        <div className = {style.div_input_block}>
+                            <label className = {style.input_label} htmlFor = "description">Description:
+                                <span className = {style.label_span}> up to 150 chars</span>
+                            </label>
+                            <input className = {style.input_form_description}
+                                   maxlength = {150}
+                                   name = "description"
+                                   placeholder = "brown fox jumps over a lazy dog. DJs flock by when jhkjk jhgMTV ax quiz prog. Junk MTV quiz graced by fox whelps. Bawds jog, flick quartz, vex nymphs."/>
+                        </div>
+                        <div className = {style.div_input_block}>
+                            <label className = {style.input_label} htmlFor = "location">Location:</label>
+                            <input className = {style.input_form_location}
+                                   name = "location"
+                                   placeholder = "Florentin Street, Tel Aviv"/>
+                        </div>
                     </div>
                     <div className = {style.div_upload_side}>
                         <img className = {style.img_scale} src = {IMAGE_SCALE}/>
-                        <div className = "d-flex row">
+                        <div className = {style.div_upload_block}>
                             <div className = {style.div_upload_side_left}>
                                 <div className = {style.div_dropzone}>
                                     <i className = {`fas fa-file-upload ${style.icon_upload}`}/>
@@ -42,14 +90,19 @@ const LostFoundForm = () =>
                     </div>
                 </section>
                 <section className = {style.section_footer_contact}>
-                    <input/>
-                    <input/>
-                    <input/>
-                    <div name = {"profile"}>Name Surname</div>
-                    <button>
-                        <i className = {`fas fa-paw ${style.button_icon}`}/>
-                        Publish
-                    </button>
+                    <div className = {style.div_footer_contacts}>
+                        <label className = {style.input_label} htmlFor = "contacts">Contacts:</label>
+                        <input className = {style.input_contact} name = "contacts" placeholder = "Phone*"/>
+                        <input className = {style.input_contact} name = "contacts" placeholder = "Email"/>
+                        <input className = {style.input_contact} name = "contacts" placeholder = "Facebook ID"/>
+                    </div>
+                    <div className = {style.div_footer_buttons}>
+                        <button className = {style.button_profile}>Name Surname</button>
+                        <button className = {style.button_publish}>
+                            <i className = {`fas fa-paw ${style.button_icon}`}/>
+                            Publish
+                        </button>
+                    </div>
                 </section>
             </div>
         </div>
